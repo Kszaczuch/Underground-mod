b83162ba85fb35e14675eb277a50f73c49e30303
Kszaczuch <b.kotlik2009@gmail.com>
2024-11-07 19:50:53 +0100

    Initial commit

---
+++ src/main/java/net/kszaczuch/undergroundmod/UndergroundModClient.java
@@ -0,0 +1,10 @@
+package net.kszaczuch.undergroundmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UndergroundModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
